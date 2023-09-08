313e115c8338dabaf19fb0c7d74b7758a21d3dfe
Natnael-Negash <nnegash658@gmail.com>
2023-09-08 19:03:51 +0000

    updated

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,14 +4,14 @@
 * Return: ALways 0 (Success)
 */
 int main(void)
-{ 
-int n,m;
-for (n  = 48; n <= 56; n++)
 {
-for (m = 49; m <= 57; m++)	
+int n, m;
+for (n = 48; n <= 56; n++)
+{
+for (m = 49; m <= 57; m++)
 {
 if (m > n)
-{ 
+{
 putchar(n);
 putchar(m);
 if (n != 56 || m != 57)
@@ -24,5 +24,5 @@ putchar(' ');
 }
 putchar('\n');
 return (0);
-} 
+}
 
