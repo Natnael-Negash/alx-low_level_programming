e0a53d994c2a11f558a4fd23f0ccd93da253970e
Natnael-Negash <nnegash658@gmail.com>
2023-09-26 19:53:50 +0000

    updated

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,20 +2,17 @@
 /**
 * _memset - fill a block of memory with a specific value
 *  @s: starting address of memory to be filled
-*  b: the desired value
+*  @b: the desired value
 *  @n: number of bytes to be changed
 *
 *  Return: changed array with new value for n bytes
 */
 char *_memset(char *s, char b, unsigned int n)
 {
-		int a = 0;
-for (; n > 0; a++)
+char *p = s;
+while (n--)
 {
-s[a] = b;
-n--;
+*p++ = b;
 }
 return (s);
 }
-
-
