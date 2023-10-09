e56f02989c1a0146824f9544bcc47e3c361e7741
Natnael-Negash <nnegash658@gmail.com>
2023-10-09 05:49:23 -0700

    updated

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog -a dog information
+ * @name : 1st member
+ * @age : 2nd member
+ * @owner:third member
+ *
+ *description : longer description
+ */
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+char *name;
+float age;
+char *owner;
 };
-
+#endif
 
