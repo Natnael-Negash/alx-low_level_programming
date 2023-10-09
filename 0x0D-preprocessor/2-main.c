089672c7b933931525dd09a6228c593e965d2835
Natnael-Negash <nnegash658@gmail.com>
2023-10-09 02:38:01 -0700

    updated

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-/* This program prints the name of the file it was compiled from*/
+/* This program prints the name of the file it was compiled from
+ *
+ * Return: Always 0 (Success)
+ */
 int main ()
 {
 	printf ("%s\n",__FILE__);
