8270c1f05ec89a6ff7fcd7ec30fcef5dd2695517
Natnael-Negash <nnegash658@gmail.com>
2023-10-11 05:46:33 -0700

    updated

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,14 +3,14 @@
 /**
  * print_name - prints a string using a function pointer
  * @name: pointer to a character array representing the name to be printed
- * @f: pointer to a function that takes a pointer to a character array as an argument and returns nothing
+ * @f: pointer to a function that takes a pointer to a character array
  *
- * This function takes a pointer to a character array and a function pointer as arguments.
+ * This function takes a pointer to a character array and a function point
  * If either argument is NULL, the function returns without doing anything.
- * If both arguments are not NULL, the function calls the function pointed to by the function pointer,
+ * If both arguments are not NULL, the function calls the function pointe
  * passing the name argument as its argument.
  */
-void print_name(char *name, void (*f)(char*))
+void print_name(char *name, void (*f)(char *))
 	{
 	if (name == NULL || f == NULL)
 		return;
